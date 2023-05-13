2a87fb16d0f0561f1cdc643973a19519902ca559
Karun Agarwal <karun.agarwal@esmagico.in>
2023-05-13 20:48:25 +0530

    Basic Voice Recognition and Prompt Flow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useEffect } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
+  const classifyItem = async ({ text }) => {
+    const prompt = `Get the item of the last query using the below examples. Return null and only null if the question is incomplete.
+
+    Query: Search for Shahi Paneer
+    Shahi Paneer
+    
+    Query: Order Aloo ki sabzi 
+    Aloo ki sabzi
+    
+    Query: I want to eat baingan ka bharta
+    Baingan ka bharta
+    
+    Query: I am craving for khichdi
+    Khichdi 
+    
+    Query: Let me have Pizza
+    Pizza 
+    
+    Query: Recommend me places which serve good waffles
+    Waffles
+    
+    Query: Good restaurants that serve healthy food
+    Healthy food
+    
+    Query: Order for me pasta
+    Pasta
+    
+    Query: Best tomato soup near me
+    Tomato soup
+    
+    Query: Ice cream shops in Koramangala
+    Ice cream
+    
+    Query: Best samosa places in Bangalore
+    
+    ${text}`;
+
+    const res = await axios.post(
+      "https://79lm3hpao7.execute-api.ap-south-1.amazonaws.com/default/fetchAnswerFromChatGPT",
+      {
+        question: prompt,
+      }
+    );
+
+    if (res?.data?.answer === "null") {
+      throw new Error();
+    }
+
+    return res?.data?.answer;
+  };
+
+  const mutation = useMutation({
+    mutationFn: classifyItem,
+  });
+
+  const { transcript, listening, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  useEffect(() => {
+    if (!listening && transcript) {
+      mutation.mutate({ text: transcript });
+    }
+  }, [transcript, listening]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-center items-center h-screen">
+        {mutation?.isLoading ? (
+          "Processing your request"
+        ) : mutation?.isError ? (
+          <div>
+            Please retry &nbsp;
+            <KeyboardVoiceIcon
+              fontSize="large"
+              className="bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 cursor-pointer"
+              onClick={SpeechRecognition.startListening}
+            />
+          </div>
+        ) : null}
+
+        {mutation?.isSuccess
+          ? `We were able to recognize that you are trying to order ${mutation?.data}`
+          : null}
+        {listening ? (
+          "Listening..."
+        ) : mutation?.status === "idle" ? (
+          <KeyboardVoiceIcon
+            fontSize="large"
+            className="bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 cursor-pointer"
+            onClick={SpeechRecognition.startListening}
+          />
+        ) : null}
+      </div>
     </div>
   );
 }
